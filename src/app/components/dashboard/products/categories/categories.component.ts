852c89e2496b2569834fb918978efd6d828785b4
Nitesh <nit90esh@gmail.com>
2019-02-12 01:46:32 +0530

    changes

--- src/app/components/dashboard/products/categories/categories.component.ts
+++ src/app/components/dashboard/products/categories/categories.component.ts
@@ -3,7 +3,6 @@ import { FormGroup, Validators, FormControl } from '@angular/forms';
 import { Error } from 'src/app/interfaces/error';
 import { Title } from '@angular/platform-browser';
 import { ScriptService } from 'src/app/services/script.service';
-import { StyleService } from 'src/app/services/style.service';
 declare const jQuery: any;
 @Component({
   selector: 'app-categories',
@@ -12,20 +11,16 @@ declare const jQuery: any;
 })
 export class CategoriesComponent implements OnInit {
   categoryForm: FormGroup;
+  selParCategories: String[];
   error: Error;
   constructor(
     private title: Title,
-    private scriptService: ScriptService,
-    private styleService: StyleService) {
+    private scriptService: ScriptService) {
     this.title.setTitle('v-kart Categories');
+    this.selParCategories = [];
   }
   ngOnInit() {
     this.initCategoryForm();
-    this.styleService.load('bootstrapSelect').then((data) => {
-      console.log('loaded: ', data);
-    }).catch((err) => {
-      console.error('not loaded......', err);
-    });
     this.scriptService.load('popper', 'bootstrapMultiselect').then((data) => {
       console.log('loaded: ', data);
       jQuery('#parent_product_catagory').bsMultiSelect();
@@ -42,5 +37,8 @@ export class CategoriesComponent implements OnInit {
   }
   saveCategory() {
     console.log('categoryForm: ', this.categoryForm.value);
+  }
+  onSelParCat(event) {
+    console.log('event: ', event);
   }
 }
